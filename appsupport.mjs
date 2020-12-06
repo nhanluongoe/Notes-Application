b30f28bfcc2e8dee47714937ac4786260a629e90
NhanLuong <nstball@gmail.com>
2020-12-06 15:35:49 +0700

    added bug tracer

--- appsupport.mjs
+++ appsupport.mjs
@@ -1,82 +1,88 @@
-import { port } from './app.mjs';
+import { port } from "./app.mjs";
 
 /**
  * Normalize a port into a number, string, or false.
  */
 export function normalizePort(val) {
-    const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-    if (port >= 0) {
-        // port number
-        return port;
-    }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 /**
  * Event listener for HTTP server "error" event.
  */
+import { dbgerror } from "./app.mjs";
 export function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  dbgerror(error);
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-    const bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(`${bind} requires elevated privileges`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(`${bind} is already in use`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 /**
  * Event listener for HTTP server "listening" event.
  */
-import { server } from './app.mjs';
+import { server, debug } from "./app.mjs";
 export function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    console.log(`Listening on ${bind}`);
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug(`Listening on ${bind}`);
 }
 
-
 export function handle404(req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 }
 
 export function basicErrorHandler(err, req, res, next) {
-    // Defer to built-in error handler if headersSent
-    // See: http://expressjs.com/en/guide/error-handling.html
-    if (res.headersSent) {
-        console.log(`basicErrorHandler HEADERS SENT error ${util.inspect(err)}`);
-        return next(err)
-    }
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // Defer to built-in error handler if headersSent
+  // See: http://expressjs.com/en/guide/error-handling.html
+  if (res.headersSent) {
+    console.log(`basicErrorHandler HEADERS SENT error ${util.inspect(err)}`);
+    return next(err);
+  }
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 }
+
+process.on("uncaughtException", function (err) {
+  console.error(`I've crashed!!! - ${err.stack || err}`);
+});
+
+import * as util from "util";
+process.on("unhandledRejection", (reason, p) => {
+  console.error(`Unhandle Rejection at: ${util.inspect(p)} reason: ${reason}`);
+});
